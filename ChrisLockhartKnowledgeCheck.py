235f8f84b8d3baa8d54af9bc775eaf2026465c7b
Chris Lockhart <chrislockhartky@gmail.com>
2023-02-19 15:05:31 -0500

    Knowledge Check for Chris Lockhart

---
+++ ChrisLockhartKnowledgeCheck.py
@@ -0,0 +1,14 @@
+print ("hello world!")
+
+ListOpinions=["Lists are fun","Lists are ok", "I hate lists"]
+print(ListOpinions[2])
+
+CelebrityBirthdays={
+    "Kevin Costner": "January 18th",
+    "Tom Hanks": "July 9th"
+    }
+print(CelebrityBirthdays["Tom Hanks"])
+
+CoolShapes=("Circle", "Square", "Triangle", "Oval")
+print(CoolShapes[2])
+
