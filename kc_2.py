a0dceb85ecd06f70df271223636ab8ecce495a73
Chris Lockhart <chrislockhartky@gmail.com>
2023-03-12 21:00:21 -0400

    Knowledge check commit

--- kc_2.py
+++ kc_2.py
@@ -1,2 +1,13 @@
-#test file to test my ability to upload via git bash
-#for knowledge check 2
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 12 19:45:31 2023
+
+@author: chris
+"""
+#A program that shows the relationship between zipcodes and Health Code scores in Louisville
+#Run the program in Spyder and make sure to have the panes tab open.
+#
+import pandas as pd
+import seaborn as sns
+df=pd.read_csv('assets/LouisvilleHealthScores.csv')
+sns.scatterplot(data=df, x=df['Zip'], y=df['score'])
